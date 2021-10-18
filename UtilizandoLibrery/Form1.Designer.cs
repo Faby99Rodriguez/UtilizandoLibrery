790aa57ec80abccd62a9266785d354504cefc66b
Fabiola_Rodríguez <Fabiola_Rodrguez@LAPTOP-6EF7R5V9>
2021-10-18 00:51:22 -0600

    Add project files.

---
+++ UtilizandoLibrery/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace UtilizandoLibrery
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txbxValMod1 = new System.Windows.Forms.TextBox();
+            this.txbxValMod2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resultado MOD:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Resultado Division:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor 1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Valor 2:";
+            // 
+            // txbxValMod1
+            // 
+            this.txbxValMod1.Location = new System.Drawing.Point(141, 94);
+            this.txbxValMod1.Name = "txbxValMod1";
+            this.txbxValMod1.Size = new System.Drawing.Size(100, 23);
+            this.txbxValMod1.TabIndex = 6;
+            // 
+            // txbxValMod2
+            // 
+            this.txbxValMod2.Location = new System.Drawing.Point(141, 173);
+            this.txbxValMod2.Name = "txbxValMod2";
+            this.txbxValMod2.Size = new System.Drawing.Size(100, 23);
+            this.txbxValMod2.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(85, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(229, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "SOLUCIÓN DE DIVISIÓN Y RESIDUO(MOD)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.ClientSize = new System.Drawing.Size(412, 394);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txbxValMod2);
+            this.Controls.Add(this.txbxValMod1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txbxValMod1;
+        private System.Windows.Forms.TextBox txbxValMod2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ UtilizandoLibrery/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UtilizandoLibrery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valor1 = double.Parse(txbxValMod1.Text);
+            double valor2 = double.Parse(txbxValMod2.Text);
+
+           
+            label1.Text = operaciones.Residuo.Mostrar(valor1, valor2);
+
+            label2.Text = operaciones.Division.MostrarDiv(valor1, valor2);
+        }
+    }
+}
